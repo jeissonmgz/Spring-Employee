57182af9b7b6fb8b14e3caf641d015a1b731eb39
jeisson-gml <jeissonm@gmlsoftware.com>
2020-09-20 20:15:35 -0500

    Fix initial test

--- src/test/java/com/jeissonmgz/employee/EmployeeApplicationTests.java
+++ src/test/java/com/jeissonmgz/employee/EmployeeApplicationTests.java
@@ -1,13 +1,16 @@
 package com.jeissonmgz.employee;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+	assertThat(true).isTrue();
+    }
 
 }
