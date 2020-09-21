e0dbac015193d885b3e4a59589eb7c2cb9dcbbb4
jeisson-gml <jeissonm@gmlsoftware.com>
2020-09-20 20:22:59 -0500

    Set database in test

--- src/test/java/com/jeissonmgz/employee/EmployeeApplicationTests.java
+++ src/test/java/com/jeissonmgz/employee/EmployeeApplicationTests.java
@@ -4,8 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 class EmployeeApplicationTests {
 
     @Test
